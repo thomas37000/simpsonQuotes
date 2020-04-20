805b79fd5b5fc77020061fe8626a5ed0386a6e8f
thomas <thomas.chalanson@sfr.fr>
2020-04-20 12:15:04 +0200

    exo finish

---
+++ simpsonQuotes/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import QuotesCard from './component/Simspon';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <QuotesCard />
+      </div>
+    );
+  }
+}
+
+export default App;
